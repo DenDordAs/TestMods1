e192a2913ab2fc4a1ed20adb5fe37466728b816d
DenDordAs <91894510+DenDordAs@users.noreply.github.com>
2025-09-16 21:04:25 +0300

    Перше завантаження

---
+++ src/main/java/com/hk_3/HK_3Client.java
@@ -0,0 +1,10 @@
+package com.hk_3;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HK_3Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/hk_3/HK_3DataGenerator.java
@@ -0,0 +1,11 @@
+package com.hk_3;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HK_3DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

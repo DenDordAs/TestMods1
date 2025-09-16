081b6bcb43bbdff777989cc1cc6035227fb54a86
DenDordAs <91894510+DenDordAs@users.noreply.github.com>
2025-09-16 21:07:05 +0300

    Додаю тестовий текст

--- src/main/java/com/hk_3/HK_3.java
+++ src/main/java/com/hk_3/HK_3.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//Тест
 public class HK_3 implements ModInitializer {
 	public static final String MOD_ID = "hk_3";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
